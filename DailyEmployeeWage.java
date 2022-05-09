2d5f267e58a8d0801c54da9fcdad706e44827b74
Polumatisrilakshmi <polumatisrilakshmi@gmail.com>
2022-05-09 19:34:46 +0530

    Added:Added Employee Attendance

--- DailyEmployeeWage.java
+++ DailyEmployeeWage.java
@@ -1,28 +1,27 @@
 package COM.BRIDGELABZ;
-
 public class DailyEmployeeWage {
     static final int WAGE_PER_HOUR = 20;
-        static final int Full_DAY_HOUR = 8;
+    static final int Full_DAY_HOUR = 8;
 
-        public void fullTime(){
-            int dailyEmpWage;
-            double attendance = Math.floor(Math.random() * 10) % 2;
+    public void fullTime(){
+        int dailyEmpWage;
+        double attendance = Math.floor(Math.random() * 10) % 2;
 
-            if (attendance == 0) {
-                System.out.println("Employee is Absent");
-                dailyEmpWage = 0;
-                System.out.println("Daily Employee Wage is :" + dailyEmpWage);
-            } else {
-                System.out.println("Employee is Present");
-                dailyEmpWage = WAGE_PER_HOUR * Full_DAY_HOUR;
-                System.out.println("Daily Employee Wage is :" + dailyEmpWage);
-            }
+        if (attendance == 0) {
+            System.out.println("Employee is Absent");
+            dailyEmpWage = 0;
+            System.out.println("Daily Employee Wage is :" + dailyEmpWage);
+        } else {
+            System.out.println("Employee is Present");
+            dailyEmpWage = WAGE_PER_HOUR * Full_DAY_HOUR;
+            System.out.println("Daily Employee Wage is :" + dailyEmpWage);
         }
-        public static void main(String[] args) {
-            DailyEmployeeWage emp = new DailyEmployeeWage();
-            emp.fullTime();
-        }
-
     }
+    public static void main(String[] args) {
+        DailyEmployeeWage emp = new DailyEmployeeWage();
+        emp.fullTime();
+    }
+
+}
 
 
